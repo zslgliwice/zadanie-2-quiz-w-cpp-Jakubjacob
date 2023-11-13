a15d43308ed78ab2562599ec6b06321f0c5537b2
Macius69 <144207804+Jakubjacob@users.noreply.github.com>
2023-11-13 21:37:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,109 @@
 #include <iostream>
-#include <windows.h>
 #include <cstdlib>
-
+#include <Windows.h>
 using namespace std;
-
-string imie;
-string odp;
 
 int main()
 {
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
     int pkt = 0;
+    string odpowiedz;
 
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
-
-    cout << imie << " witaj w tescie z programowania\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
-    }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
-
-    }
     Sleep(3000);
     system("cls");
 
+    cout << "witaj w Tescie piłkarskim" << endl;
+    cout << "Ile złotych pilek wygrał RONALDO:" << endl;
+    cout << "a) 1." << endl;
+    cout << "b) 3." << endl;
+    cout << "c) 5." << endl;
+    cin >> odpowiedz;
+    if (odpowiedz == "c")
+    {
+        cout << " odpowiedziales poprawnie" << endl;
+        pkt++;
+        Sleep(3000);
+        system("cls");
+    }
+    else
+    {
+        cout << " poprawna odpowiedź to c" << endl;
+        Sleep(3000);
+        system("cls");
+    }
 
 
-    return 0;
 }
+    cout << "Ile ligi mistrzów wygraL Real madrid:" << endl;
+    cout << "a 14." << endl;
+    cout << "b) 9." << endl;
+    cout << "c) 3." << endl;
+    cin >> odpowiedz;
+    if (odpowiedz == "a")
+    {
+        cout << " odpowiedziales poprawnie" << endl;
+        pkt++;
+        Sleep(3000);
+        system("cls");
+    }
+    else
+    {
+        cout << " poprawna odpowiedź to a" << endl;
+        Sleep(3000);
+        system("cls");
+    }
+    cout << "Jak nazywa sie Polska liga piłkarska:" << endl;
+    cout << "a Chicago Bulls." << endl;
+    cout << "b)Ekstraklasa." << endl;
+    cout << "c) PolscyKopacze." << endl;
+    cin >> odpowiedz;
+    if (odpowiedz == "b")
+    {
+        cout << " odpowiedziales dobrze" << endl;
+        pkt++;
+        Sleep(3000);
+        system("cls");
+    }
+    else
+    {
+        cout << " poprawna odpowiedź to b " << endl;
+        Sleep(3000);
+        system("cls");
+    }
+    cout << "Jak nazywa sie Polski gracz Barcelony?" << endl;
+    cout << "a) Robert Lewandowski." << endl;
+    cout << "b) Arkadiusz MILIK." << endl;
+    cout << "c) Zbigniew Ziobro." << endl;
+    cin >> odpowiedz;
+    if (odpowiedz == "a")
+    {
+        cout << " odpowiedziales poprawnie" << endl;
+        pkt++;
+        Sleep(3000);
+        system("cls");
+    }
+    else
+    {
+        cout << " poprawna odpowiedź to a" << endl;
+        Sleep(3000);
+        system("cls");
+    }
+    cout << "Ile Barcelona ma wygranych lig Mistrzów:" << endl;
+    cout << "a) 0" << endl;
+    cout << "b) Mniej od Realu ahahah" << endl;
+    cout << "c) 0" << endl;
+    cin >> odpowiedz;
+    if (odpowiedz == "b")
+    {
+        cout << " odpowiedziales poprawnie" << endl;
+        pkt++;
+        Sleep(3000);
+        system("cls");
+    }
+    else
+    {
+        cout << " poprawna odpowiedź to b" << endl;
+        Sleep(3000);
+        system("cls");
+    }
+    cout << "ilosc zdobytych punktow: " << pkt << endl;
+    return 0;
